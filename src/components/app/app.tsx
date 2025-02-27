17415415b4eeab4ab9a7b2a4902195d892d1da28
Farit <afroid13@gmail.com>
2025-02-27 17:25:27 +0400

    feat: Добавлен автоматический скролл вверх при переходе на другую страницу

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,22 +4,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import Layout from '../layout/layout';
 import ProductPage from '../pages/product-page/product-page';
+import ScrollToTop from '../scroll-to-top/scroll-to-top';
 
 function App(): JSX.Element {
   return (
     <HelmetProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path={AppRoute.Index} element={<Layout/>}>
-            <Route path={AppRoute.Index}>
-              <Route index element={<CatalogPage/>}/>
-              <Route
-                path={AppRoute.Product}
-                element={<ProductPage/>}
-              />
+        <ScrollToTop>
+          <Routes>
+            <Route path={AppRoute.Index} element={<Layout/>}>
+              <Route path={AppRoute.Index}>
+                <Route index element={<CatalogPage/>}/>
+                <Route
+                  path={AppRoute.Product}
+                  element={<ProductPage/>}
+                />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </HelmetProvider>
   );
