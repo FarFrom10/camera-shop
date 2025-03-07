481862ce460e0b8ed78999cc941a8dc94c5a5e03
Farit <afroid13@gmail.com>
2025-03-07 17:24:32 +0400

    feat: Реализация focus trap для общего модального окна - modal-wrapper-content

--- src/components/modal-wrapper-content/modal-wrapper-content.tsx
+++ src/components/modal-wrapper-content/modal-wrapper-content.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { CLASS_SCROLL_LOCK, IconName } from '../../const';
 import CommonIcon from '../common-icon/common-icon';
 
@@ -9,7 +9,21 @@ type ModalWrapperContentProps = {
 
 function ModalWrapperContent({onModalClose, children}: ModalWrapperContentProps): JSX.Element {
   //Компонент существует с той целью, чтобы в useEffect срабатывала cleanUp функция
+  const modalRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
+    const modalElement = modalRef.current as HTMLDivElement;
+    const focusableElements = modalElement.querySelectorAll(
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+    );
+    const firstFocusableElement = focusableElements[0];
+    const lastFocusableElement = focusableElements[focusableElements.length - 1];
+
+    //Без этого не получилось добавить фокус на модальном окне при его появлении
+    setTimeout(() => {
+      (firstFocusableElement as HTMLElement)?.focus();
+    }, 300) ;
+
     const closeModal = (evt: KeyboardEvent) => {
       if(evt.key === 'Escape'){
         onModalClose();
@@ -19,8 +33,23 @@ function ModalWrapperContent({onModalClose, children}: ModalWrapperContentProps)
     window.addEventListener('keydown', closeModal);
     document.body.classList.add(CLASS_SCROLL_LOCK);
 
+    const handleTabKey = (event: KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        if (document.activeElement === lastFocusableElement) {
+          event.preventDefault();
+          (firstFocusableElement as HTMLElement)?.focus();
+        } else if (document.activeElement === firstFocusableElement && event.shiftKey) {
+          event.preventDefault();
+          (firstFocusableElement as HTMLElement)?.focus();
+        }
+      }
+    };
+
+    document.addEventListener('keydown', handleTabKey);
+
     return () =>{
       window.removeEventListener('keydown', closeModal);
+      document.removeEventListener('keydown', handleTabKey);
       document.body.classList.remove(CLASS_SCROLL_LOCK);
     };
 
@@ -28,11 +57,11 @@ function ModalWrapperContent({onModalClose, children}: ModalWrapperContentProps)
 
 
   return(
-    <div className="modal__content">
-      {children}
+    <div ref={modalRef} className="modal__content">
       <button onClick={onModalClose} className="cross-btn" type="button" aria-label="Закрыть попап">
         <CommonIcon icon={IconName.Close}/>
       </button>
+      {children}
     </div>
   );
 }
