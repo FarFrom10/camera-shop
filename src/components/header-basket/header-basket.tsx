18d5a9569014bb61b68666fa32963382668cb061
Farit <afroid13@gmail.com>
2025-05-13 22:10:26 +0400

    feat: В шапке сайта, на иконке корзины, отображается кол-во товаров

--- src/components/header-basket/header-basket.tsx
+++ src/components/header-basket/header-basket.tsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
 import { AppRoute, IconName } from '../../const';
 import CommonIcon from '../common-icon/common-icon';
+import { useAppSelector } from '../../hooks';
+import { selectAddedCameras } from '../../store/basket-process/basket-process.selectors';
 
 function HeaderBasket(): JSX.Element {
+  const addedCameras = useAppSelector(selectAddedCameras);
+  const totalAmount = addedCameras.reduce((sum, item) => sum + item.amount, 0);
+  const displayAmount = totalAmount > 99 ? 99 : totalAmount;
+
   return (
     <Link data-testid='headerBasket' className="header__basket-link" to={AppRoute.Basket}>
       <CommonIcon icon={IconName.CartAdded}/>
-      {/* <span className="header__basket-count">3</span> */}
+      {!!totalAmount && <span className="header__basket-count">{displayAmount}</span>}
     </Link>
   );
 }
